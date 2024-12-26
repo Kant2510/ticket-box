f282753d98fba0e8448dbb52173370df4f898766
JakePham23 <jakeptnn@gmail.com>
2024-12-26 18:26:50 +0700

    fix error

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ dotenv.config()
 import app from './src/app.js'
 const PORT =  3005;
 
-// const server = app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// })
-//
-// process.on('SIGINT', (err, promise) => {
-//     server.close(() => {
-//         console.log(`Server stopped`);
-//         process.exit(1);
-//     })
-// })
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
 
-<<<<<<< HEAD
 process.on('SIGINT', (err, promise) => {
     server.close(() => {
         console.log(`Server stopped`);
@@ -22,7 +14,3 @@ process.on('SIGINT', (err, promise) => {
     })
 })
 
-// export default app;
-=======
-export default app;
->>>>>>> 1bf3b34a1f5056dbed472829fc8adefee6a3f0df
