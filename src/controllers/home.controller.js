16f645b2022ef16b64bf8139e425290d0579aa34
JakePham23 <jakeptnn@gmail.com>
2024-12-26 15:47:29 +0700

    change to deploy app

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -3,19 +3,19 @@ import TicketTypeService from '../services/ticket_type.service.js'
 
 const getHomepage = async (req, res) => {
     try {
-        const events = await EventService.findRecomendedEvents()
-        const recommendTicketTypes = await TicketTypeService.findByRecommend()
-        const newReleaseTicketTypes = await TicketTypeService.findByNewRelease()
-        // get the first of recommendedTicketTypes and the 3 first of newReleaseTicketTypes
-        const nowShowing = [recommendTicketTypes[0]].concat(newReleaseTicketTypes.slice(0, 3))
-        console.log('nowShowing:', nowShowing)
-        if(req.session.customer){
-            const customer = req.session.customer;
-            res.render('index', { customer, events, nowShowing })
-        } else{
-            res.render('index', { customer: null, events, nowShowing })
-
-        }
+        // const events = await EventService.findRecomendedEvents()
+        // const recommendTicketTypes = await TicketTypeService.findByRecommend()
+        // const newReleaseTicketTypes = await TicketTypeService.findByNewRelease()
+        // // get the first of recommendedTicketTypes and the 3 first of newReleaseTicketTypes
+        // const nowShowing = [recommendTicketTypes[0]].concat(newReleaseTicketTypes.slice(0, 3))
+        // console.log('nowShowing:', nowShowing)
+        // if(req.session.customer){
+        //     const customer = req.session.customer;
+        //     res.render('index', { customer, events, nowShowing })
+        // } else{
+        //     res.render('index', { customer: null, events, nowShowing })
+        // }
+        res.render('index', { customer: null , events: null, nowShowing: null })
     } catch (error) {
         res.status(500).json({ error: 'Failed to load events' })
     }
