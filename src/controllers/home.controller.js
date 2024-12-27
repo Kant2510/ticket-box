c08c190e82b16e9cd0604c6c377648bb86b5aa00
JakePham23 <jakeptnn@gmail.com>
2024-12-27 23:06:44 +0700

    test deploy

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,23 +2,19 @@ import EventService from '../services/event.service.js'
 import TicketTypeService from '../services/ticket_type.service.js'
 
 const getHomepage = async (req, res) => {
-    // try {
-    //     const events = await EventService.findRecomendedEvents()
-    //     const recommendTicketTypes = await TicketTypeService.findByRecommend()
-    //     const newReleaseTicketTypes = await TicketTypeService.findByNewRelease()
-    //     // get the first of recommendedTicketTypes and the 3 first of newReleaseTicketTypes
-    //     const nowShowing = [recommendTicketTypes[0]].concat(newReleaseTicketTypes.slice(0, 3))
-    //     console.log('nowShowing:', nowShowing)
-    //     if(req.session.customer){
-    //         const customer = req.session.customer;
-    //         res.render('index', { customer, events, nowShowing })
-    //     } else{
-    //         res.render('index', { customer: null, events, nowShowing })
-    //     }
-    // } catch (error) {
-    //     res.status(500).json({ error: 'Failed to load events' })
-    // }
-    return res.render('index')
+        const events = await EventService.findRecomendedEvents()
+        const recommendTicketTypes = await TicketTypeService.findByRecommend()
+        const newReleaseTicketTypes = await TicketTypeService.findByNewRelease()
+        // get the first of recommendedTicketTypes and the 3 first of newReleaseTicketTypes
+        const nowShowing = [recommendTicketTypes[0]].concat(newReleaseTicketTypes.slice(0, 3))
+        console.log('nowShowing:', nowShowing)
+        try{
+            const customer = req.session.customer;
+            res.render('index', { customer, events, nowShowing })
+
+    } catch (error) {
+            res.render('index', { customer: null, events, nowShowing })    }
+    // return res.render('index')
 }
 
 export default getHomepage
